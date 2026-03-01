6dffcb7f2f27ea3616044627afc8a65fce3b3c02
Jan <jplacht@gmail.com>
2026-03-01 13:49:26 +0100

    improve(celery): log once child process spawns or shuts down

--- backend/core/celery.py
+++ backend/core/celery.py
@@ -1,7 +1,8 @@
 import logging.config
 
+import structlog
 from celery import Celery
-from celery.signals import setup_logging
+from celery.signals import setup_logging, worker_process_init, worker_process_shutdown
 from django_structlog.celery.steps import DjangoStructLogInitStep
 
 app = Celery('prunplanner')
@@ -17,6 +18,19 @@ def config_loggers(*args, **kwargs):
     from django.conf import settings
 
     logging.config.dictConfig(settings.LOGGING)
+
+
+logger = structlog.get_logger()
+
+
+@worker_process_init.connect
+def log_new_process(**kwargs):
+    logger.info('worker_child_process_spawned')
+
+
+@worker_process_shutdown.connect
+def log_shutdown_process(**kwargs):
+    logger.info('worker_child_process_shutdown')
 
 
 app.autodiscover_tasks()
