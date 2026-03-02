ffbce743f84440191395fe9d9d612d8d92d52483
Jan <jplacht@gmail.com>
2026-03-02 09:21:12 +0100

    improve(api): trailing slash on all urls

--- backend/gamedata/api/urls.py
+++ backend/gamedata/api/urls.py
@@ -22,23 +22,25 @@ urlpatterns = [
     ),
     path('recipes/', GameRecipeViewSet.as_view(actions={'get': 'list'}), name='recipe-list'),
     path('buildings/', GameBuildingViewSet.as_view(actions={'get': 'list'}), name='building-list'),
-    path('planets/multiple', GamePlanetViewSet.as_view({'post': 'multiple'}), name='planet-multiple'),
-    path('planets/search', GamePlanetViewSet.as_view({'post': 'search'}), name='planet-search'),
+    path('planets/multiple/', GamePlanetViewSet.as_view({'post': 'multiple'}), name='planet-multiple'),
+    path('planets/search/', GamePlanetViewSet.as_view({'post': 'search'}), name='planet-search'),
     path(
-        'planet/<str:planet_natural_id>/popr',
+        'planet/<str:planet_natural_id>/popr/',
         GamePlanetViewSet.as_view({'get': 'latest_popr'}),
         name='planet-infrastructure',
     ),
-    path('planet/<str:planet_natural_id>', GamePlanetViewSet.as_view({'get': 'retrieve'}), name='planet-detail'),
-    path('planets/<str:search_term>', GamePlanetViewSet.as_view({'get': 'search_single'}), name='planet-search-single'),
+    path('planet/<str:planet_natural_id>/', GamePlanetViewSet.as_view({'get': 'retrieve'}), name='planet-detail'),
+    path(
+        'planets/<str:search_term>/', GamePlanetViewSet.as_view({'get': 'search_single'}), name='planet-search-single'
+    ),
     path('planets/', GamePlanetViewSet.as_view({'get': 'list'}), name='planet-list'),
     path(
-        'cxpc/<str:ticker>/<str:exchange_code>',
+        'cxpc/<str:ticker>/<str:exchange_code>/',
         ExchangeCXPCViewSet.as_view({'get': 'cxpc_exchange_data'}),
         name='cxpc-market-data-full',
     ),
     path(
-        'cxpc/<str:ticker>',
+        'cxpc/<str:ticker>/',
         ExchangeCXPCViewSet.as_view({'get': 'cxpc_ticker_data'}),
         name='cxpc-market-data-ticker',
     ),
