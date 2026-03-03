1472dd9d31a33cfc909d31784b29baaacc739326
Jan <jplacht@gmail.com>
2026-03-03 14:27:10 +0100

    improve(gunicorn): add gunicorn config

---
+++ backend/gunicorn.conf.py
@@ -0,0 +1,24 @@
+import os
+
+# basic
+bind = '0.0.0.0:8000'
+pythonpath = 'backend'
+wsgi_app = 'core.wsgi:application'
+
+# worker
+workers = int(os.getenv('GUNICORN_WORKERS', 3))
+threads = int(os.getenv('GUNICORN_THREADS', 2))
+worker_class = 'gthread'
+prelead_app = True
+
+# restart logic
+max_requests = 1000
+max_requests_jitter = 100
+
+# logging
+accesslog = '-'
+errorlog = '-'
+loglevel = 'info'
+
+# timeout
+timeout = 30
