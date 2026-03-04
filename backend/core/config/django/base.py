6eebb12173f991fd353d9fd9a3920e74b337a86c
Jan <jplacht@gmail.com>
2026-03-04 16:16:24 +0100

    fix(django): allowed hosts from .env

--- backend/core/config/django/base.py
+++ backend/core/config/django/base.py
@@ -21,7 +21,7 @@ SECRET_KEY = settings.django_secret_key
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS: list[str] = []
+ALLOWED_HOSTS = settings.django_allowed_hosts.split(',')
 
 
 # Application definition
