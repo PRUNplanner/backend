e5e555c65162ce8902345b693005c9fc0f3eba62
Jan <jplacht@gmail.com>
2026-04-08 08:10:28 +0200

    chore(py): F401

--- backend/user/models/__init__.py
+++ backend/user/models/__init__.py
@@ -3,3 +3,13 @@ from .preferences import UserPreference
 from .user import User
 from .verification_codes import VerificationCode, VerificationeCodeChoices
 from .configs import GlobalConfigWebhook, WebhookSenderChoices
+
+__all__ = [
+    "UserAPIKey",
+    "UserPreference",
+    "User",
+    "VerificationCode",
+    "VerificationeCodeChoices",
+    "GlobalConfigWebhook",
+    "WebhookSenderChoices",
+]
